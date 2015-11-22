be72a46903cbdd589c18e8be14943d572f4c1d36
Kshitijh Meelu <kmeelu.52@gmail.com>
2015-11-22 14:52:24 -0500

    Implement Queue structure

---
+++ queue.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+__author__ = 'kmeelu'
+
+
+# Implementation of Queue data structure
+class Queue(object):
+    def __init__(self):
+        self.Q = []
+
+    # Enqueue value onto queue
+    def enqueue(self, val):
+        self.Q.append(val)
+
+    # Dequeue val from queue
+    def dequeue(self):
+        if self.Q:
+            val = self.Q.pop(0)
+            return val
+        else:
+            print('Error: Nothing to dequeue')
+            return 'Error'
+
+    # Check if queue empty
+    def isEmpty(self):
+        return not bool(self.Q)
+
+    # Peek at top queue value
+    def peek(self):
+        return self.Q[0]
+
+    def __str__(self):
+        return str(self.Q)
+
+
+
+# Main function includes tests for Queue
+def main():
+    q = Queue()
+
+    for i in range(10):
+        q.enqueue(i)
+        print q
+
+    print '\n'
+
+    for _ in range(7):
+        print 'Removed %i from the queue' % (q.dequeue())
+
+    print '\nFinal queue:'
+    print q
+
+
+if __name__ == '__main__':
+    main()
